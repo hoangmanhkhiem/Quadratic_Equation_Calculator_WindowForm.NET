0b86ff58d677813cd5941ecb8bf605ab43f16129
hoangmanhkhiem <khiemhm04@gmail.com>
2024-08-27 12:05:11 +0700

    Add project files.

---
+++ Quadratic Equation Calculator/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace Quadratic_Equation_Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.inputa = new System.Windows.Forms.TextBox();
+            this.inputb = new System.Windows.Forms.TextBox();
+            this.inputc = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.result = new System.Windows.Forms.TextBox();
+            this.calc = new System.Windows.Forms.Button();
+            this.reset = new System.Windows.Forms.Button();
+            this.exitbutton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(55, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "PTB2 CALC";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.label2.Location = new System.Drawing.Point(29, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nhập a";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.label3.Location = new System.Drawing.Point(29, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nhập b";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.label4.Location = new System.Drawing.Point(29, 168);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Nhập c";
+            // 
+            // inputa
+            // 
+            this.inputa.Location = new System.Drawing.Point(77, 85);
+            this.inputa.Name = "inputa";
+            this.inputa.Size = new System.Drawing.Size(132, 20);
+            this.inputa.TabIndex = 4;
+            // 
+            // inputb
+            // 
+            this.inputb.Location = new System.Drawing.Point(77, 124);
+            this.inputb.Name = "inputb";
+            this.inputb.Size = new System.Drawing.Size(132, 20);
+            this.inputb.TabIndex = 5;
+            // 
+            // inputc
+            // 
+            this.inputc.Location = new System.Drawing.Point(77, 165);
+            this.inputc.Name = "inputc";
+            this.inputc.Size = new System.Drawing.Size(132, 20);
+            this.inputc.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(99, 209);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Kết quả";
+            // 
+            // result
+            // 
+            this.result.Location = new System.Drawing.Point(29, 237);
+            this.result.Multiline = true;
+            this.result.Name = "result";
+            this.result.ReadOnly = true;
+            this.result.Size = new System.Drawing.Size(180, 103);
+            this.result.TabIndex = 8;
+            // 
+            // calc
+            // 
+            this.calc.Location = new System.Drawing.Point(32, 362);
+            this.calc.Name = "calc";
+            this.calc.Size = new System.Drawing.Size(75, 23);
+            this.calc.TabIndex = 9;
+            this.calc.Text = "Kết quả";
+            this.calc.UseVisualStyleBackColor = true;
+            this.calc.Click += new System.EventHandler(this.calc_Click);
+            // 
+            // reset
+            // 
+            this.reset.Location = new System.Drawing.Point(134, 362);
+            this.reset.Name = "reset";
+            this.reset.Size = new System.Drawing.Size(75, 23);
+            this.reset.TabIndex = 10;
+            this.reset.Text = "Reset";
+            this.reset.UseVisualStyleBackColor = true;
+            this.reset.Click += new System.EventHandler(this.reset_Click_1);
+            // 
+            // exitbutton
+            // 
+            this.exitbutton.Location = new System.Drawing.Point(77, 415);
+            this.exitbutton.Name = "exitbutton";
+            this.exitbutton.Size = new System.Drawing.Size(75, 23);
+            this.exitbutton.TabIndex = 11;
+            this.exitbutton.Text = "Thoát";
+            this.exitbutton.UseVisualStyleBackColor = true;
+            this.exitbutton.Click += new System.EventHandler(this.exitbutton_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.pictureBox1.Location = new System.Drawing.Point(248, 62);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(540, 376);
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(752, 374);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(27, 23);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "+";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(752, 403);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(27, 23);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "-";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(257, 71);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(490, 356);
+            this.pictureBox2.TabIndex = 15;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox2_Paint);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(650, 28);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(138, 13);
+            this.linkLabel1.TabIndex = 16;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Author: Hoàng Mạnh Khiêm";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.exitbutton);
+            this.Controls.Add(this.reset);
+            this.Controls.Add(this.calc);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.inputc);
+            this.Controls.Add(this.inputb);
+            this.Controls.Add(this.inputa);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Quadratic Equation Calculator";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox inputa;
+        private System.Windows.Forms.TextBox inputb;
+        private System.Windows.Forms.TextBox inputc;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox result;
+        private System.Windows.Forms.Button calc;
+        private System.Windows.Forms.Button reset;
+        private System.Windows.Forms.Button exitbutton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ Quadratic Equation Calculator/Form1.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//Yêu cầu:
+//-TextBox Kết quả không cho phép người dùng nhập dữ liệu.
+//- Khi người dùng nhấn nút Giải PT (Hoặc nhấn Alt+G), chương trình sẽ kiểm tra xem người dùng đã nhập đủ a, b, c chưa; nếu chưa nhập bắt người dùng phải nhập. Nếu đã nhập đủ dữ liệu kiểm tra xem a, b, c đã nhập có phải số không. Nếu là số thì giải phương trình bậc 2 với a, b, c đã nhập và đưa kết quả vào TextBox kết quả.
+//- Khi người dùng nhấn nút Làm lại (Hoặc nhấn Alt +L), Form sẽ trở về trạng thái ban đầu (Các TextBox bị xóa trống, con trỏ đang ở TextBox Nhập a)
+//- Khi người dùng nhấn nút Thoát (hoặc nhấn Alt + T) chương trình sẽ đưa ra thông báo “Ban cố muốn thoát không?” với 2 nút Yes/No, nếu chọn Yes thì thoát.
+
+
+namespace Quadratic_Equation_Calculator
+{
+    public partial class Form1 : Form
+    {
+        private float zoomFactor = 1.0f;
+        private const float zoomStep = 0.1f;
+        private double a;
+        private double b;
+        private double c;
+        private bool check1 = false;
+        private bool checking = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calc_Click(object sender, EventArgs e)
+        {
+            string txtA = inputa.Text;
+            string txtB = inputb.Text;
+            string txtC = inputc.Text;
+            if (txtA == "")
+            {
+                MessageBox.Show("Bạn chưa nhập a", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else if (txtB == "")
+            {
+                MessageBox.Show("Bạn chưa nhập b", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else if (txtC == "")
+            {
+                MessageBox.Show("Bạn chưa nhập c", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                try
+                {
+                    a = double.Parse(txtA);
+                    b = double.Parse(txtB);
+                    c = double.Parse(txtC);
+                    if (a == 0)
+                    {
+                        if (b == 0)
+                        {
+                            if (c == 0)
+                            {
+                                result.Text = "Phương trình có vô số nghiệm";
+                            }
+                            else
+                            {
+                                result.Text = "Phương trình vô nghiệm";
+                            }
+                        }
+                        else
+                        {
+                            result.Text = "Phương trình có nghiệm duy nhất: x = " + (-c / b);
+                        }
+                    }
+                    else
+                    {
+                        double delta = b * b - 4 * a * c;
+                        if (delta < 0)
+                        {
+                            result.Text = "Phương trình vô nghiệm";
+                        }
+                        else if (delta == 0)
+                        {
+                            result.Text = "Phương trình có nghiệm kép: x1 = x2 = " + (-b / (2 * a));
+                        }
+                        else
+                        {
+                            double x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+                            double x2 = (-b - Math.Sqrt(delta)) / (2 * a);
+                            result.Text = "Phương trình có 2 nghiệm phân biệt: x1 = " + x1 + ", x2 = " + x2;
+                            checking = true;
+                        }
+                    }
+                    check1 = true;
+                    pictureBox2.Invalidate();
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("a, b, c phải là số", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+            }
+        }
+
+        private void reset_Click_1(object sender, EventArgs e)
+        {
+            inputa.Text = "";
+            inputb.Text = "";
+            inputc.Text = "";
+            result.Text = "";
+            checking = false;
+            check1 = false;
+            pictureBox2.Invalidate();
+        }
+
+        private void exitbutton_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Bạn có muốn thoát không?", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                Application.Exit();        
+        }
+
+        // Lắng nghe keyboard event
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (keyData == (Keys.Alt | Keys.G))
+            {
+                calc_Click(null, null);
+                return true;
+            }
+            else if (keyData == (Keys.Alt | Keys.L))
+            {
+                reset_Click_1(null, null);
+                return true;
+            }
+            else if (keyData == (Keys.Alt | Keys.T))
+            {
+                exitbutton_Click(null, null);
+                return true;
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            string url = "https://github.com/hoangmanhkhiem";
+            try
+            {
+                System.Diagnostics.Process.Start(new ProcessStartInfo
+                {
+                    FileName = url,
+                    UseShellExecute = true
+                });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Không thể mở liên kết: {ex.Message}", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void pictureBox2_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Pen pen = new Pen(Color.Blue, 2);
+            Pen derivativePen = new Pen(Color.Red, 2);
+            Font font = new Font("Arial", 8);
+
+            // Vẽ trục X và Y
+            g.DrawLine(Pens.Black, pictureBox1.Width / 2, 0, pictureBox1.Width / 2, pictureBox1.Height);
+            g.DrawLine(Pens.Black, 0, pictureBox1.Height / 2, pictureBox1.Width, pictureBox1.Height / 2);
+
+            for (int i = 1; i < pictureBox1.Width / (20 * zoomFactor); i++)
+            {
+                int x = pictureBox1.Width / 2 + (int)(i * 20 * zoomFactor);
+                int y = pictureBox1.Height / 2;
+                g.DrawLine(pen, x, y - 5, x, y + 5);
+                g.DrawString((i).ToString(), font, Brushes.Black, x + 2, y + 5);
+                g.DrawLine(pen, pictureBox1.Width / 2 - (int)(i * 20 * zoomFactor), y - 5, pictureBox1.Width / 2 - (int)(i * 20 * zoomFactor), y + 5);
+                g.DrawString((-i).ToString(), font, Brushes.Black, pictureBox1.Width / 2 - (int)(i * 20 * zoomFactor) + 2, y + 5);
+            }
+
+            for (int i = 1; i < pictureBox1.Height / (20 * zoomFactor); i++)
+            {
+                int x = pictureBox1.Width / 2;
+                int y = pictureBox1.Height / 2 + (int)(i * 20 * zoomFactor);
+                g.DrawLine(pen, x - 5, y, x + 5, y);
+                g.DrawString((-i).ToString(), font, Brushes.Black, x - 20, y + 2);
+                g.DrawLine(pen, x - 5, pictureBox1.Height / 2 - (int)(i * 20 * zoomFactor), x + 5, pictureBox1.Height / 2 - (int)(i * 20 * zoomFactor));
+                g.DrawString((i).ToString(), font, Brushes.Black, x - 20, pictureBox1.Height / 2 - (int)(i * 20 * zoomFactor) + 2);
+            }
+
+            PointF[] quadraticPoints = new PointF[pictureBox1.Width];
+            for (int x = 0; x < pictureBox1.Width; x++)
+            {
+                double xValue = (x - pictureBox1.Width / 2) / (20.0 * zoomFactor); // Chuyển đổi từ pixel thành giá trị x
+                double yValue = a * xValue * xValue + b * xValue + c;
+                int y = pictureBox1.Height / 2 - (int)(yValue * 20 * zoomFactor); // Chuyển đổi từ giá trị y thành pixel
+                quadraticPoints[x] = new PointF(x, y);
+                Console.WriteLine($"x = {xValue}, y = {yValue}"); // Kiểm tra giá trị
+            }
+
+            if (check1 == true)
+            {
+                g.DrawCurve(pen, quadraticPoints);
+
+                double x0 = -b / (2 * a);
+                double y0 = a * x0 * x0 + b * x0 + c;
+                float x0Pixel = (float)(pictureBox1.Width / 2 + x0 * 20 * zoomFactor);
+                float y0Pixel = (float)(pictureBox1.Height / 2 - y0 * 20 * zoomFactor);
+                g.FillEllipse(Brushes.Red, x0Pixel - 2, y0Pixel - 2, 4, 4);
+
+                double x1 = 0;
+                double y1 = a * x1 * x1 + b * x1 + c;
+                float x1Pixel = (float)(pictureBox1.Width / 2 + x1 * 20 * zoomFactor);
+                float y1Pixel = (float)(pictureBox1.Height / 2 - y1 * 20 * zoomFactor);
+                g.FillEllipse(Brushes.Red, x1Pixel - 2, y1Pixel - 2, 4, 4);
+            }
+            
+
+            if(checking == true)
+            {
+                double x_1 = (-b + Math.Sqrt(b * b - 4 * a * c)) / (2 * a);
+                double x_2 = (-b - Math.Sqrt(b * b - 4 * a * c)) / (2 * a);
+                double y_1 = a * x_1 * x_1 + b * x_1 + c;
+                double y_2 = a * x_2 * x_2 + b * x_2 + c;
+                float x_1Pixel = (float)(pictureBox1.Width / 2 + x_1 * 20 * zoomFactor);
+                float y_1Pixel = (float)(pictureBox1.Height / 2 - y_1 * 20 * zoomFactor);
+                float x_2Pixel = (float)(pictureBox1.Width / 2 + x_2 * 20 * zoomFactor);
+                float y_2Pixel = (float)(pictureBox1.Height / 2 - y_2 * 20 * zoomFactor);
+                g.FillEllipse(Brushes.Red, x_1Pixel - 2, y_1Pixel - 2, 4, 4);
+                g.FillEllipse(Brushes.Red, x_2Pixel - 2, y_2Pixel - 2, 4, 4);
+            }
+
+            pen.Dispose();
+            derivativePen.Dispose();
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("Bạn có muốn thoát không?", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No)
+                e.Cancel = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            zoomFactor += zoomStep;
+            pictureBox2.Invalidate();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (zoomFactor > zoomStep)
+            {
+                zoomFactor -= zoomStep;
+                pictureBox2.Invalidate();
+            }
+        }
+    }
+}
